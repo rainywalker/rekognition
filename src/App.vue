03e039d7daf7204dcda4b966e4fe5c5dd426b1ba
jaemin.yoon <>
2019-05-07 17:01:48 +0900

    트랜지션 적용

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,9 @@
                 <router-link :to="item.uri">{{item.label}}</router-link>
             </li>
         </ul>
-
-        <router-view></router-view>
-
+        <transition name="fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
     </v-app>
 </template>
 
@@ -64,4 +64,13 @@
             }
         }
     }
+    .fade-enter-active, .fade-leave-active {
+        transition: all 0.3s ease;
+    }
+    .fade-enter, .fade-leave-active {
+        opacity: 0;
+        transform: translateX(-30%);
+    }
+
+
 </style>
